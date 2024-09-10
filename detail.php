78d0ef8c591c4f2e02a1ac17bd53d44834b550f7
Nicholas Caporusso <ncaporusso@gmail.com>
2024-09-10 10:21:35 -0400

    Changed files into actual HTML

--- detail.php
+++ detail.php
@@ -1,1 +1,9 @@
-DETAIL
+<html>
+	<head>
+		<title>Detail</title>
+	</head>
+	<body>
+		<h1>Detail</h1>
+		<a href="index.php">Go to index</a>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-INDEX
-<a href="detail.php">Go to detail page</a>
+<html>
+	<head>
+		<title>Index</title>
+	</head>
+	<body>
+		<h1>Index</h1>
+		<a href="detail.php">Go to detail</a>
+	</body>
+</html>
